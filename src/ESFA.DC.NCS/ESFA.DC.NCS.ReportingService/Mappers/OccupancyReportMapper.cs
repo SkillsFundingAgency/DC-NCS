458f00a378e44179fbd2aca7ddb7bc44d79db802
Cory-Wynn <CoryWynn1993@gmail.com>
2020-02-27 15:05:58 +0000

    70852 changed occupancy report to reflect bau and changed filename key

--- src/ESFA.DC.NCS/ESFA.DC.NCS.ReportingService/Mappers/OccupancyReportMapper.cs
+++ src/ESFA.DC.NCS/ESFA.DC.NCS.ReportingService/Mappers/OccupancyReportMapper.cs
@@ -1,5 +1,7 @@
 ﻿using CsvHelper.Configuration;
+using ESFA.DC.NCS.Interfaces;
 using ESFA.DC.NCS.Interfaces.ReportingService;
+using ESFA.DC.NCS.Models.Reports;
 
 namespace ESFA.DC.NCS.ReportingService.Mappers
 {
@@ -19,8 +21,18 @@ namespace ESFA.DC.NCS.ReportingService.Mappers
             Map(m => m.OutcomeType).Index(i++).Name("Outcome Type");
             Map(m => m.OutcomeEffectiveDate).Index(i++).Name("Outcome Effective Date").TypeConverterOption.Format("dd/MM/yyyy");
             Map(m => m.OutcomePriorityCustomer).Index(i++).Name("Outcome Priority Group");
-            Map(m => m.Period).Index(i++).Name("Period");
-            Map(m => m.Value).Index(i++).Name("Value");
+            Map(m => m.April).Index(i++).Name("April 2019 £");
+            Map(m => m.May).Index(i++).Name("May 2019 £");
+            Map(m => m.June).Index(i++).Name("June 2019 £");
+            Map(m => m.July).Index(i++).Name("July 2019 £");
+            Map(m => m.August).Index(i++).Name("August 2019 £");
+            Map(m => m.September).Index(i++).Name("September 2019 £");
+            Map(m => m.October).Index(i++).Name("October 2019 £");
+            Map(m => m.November).Index(i++).Name("November 2019 £");
+            Map(m => m.December).Index(i++).Name("December 2019 £");
+            Map(m => m.January).Index(i++).Name("January 2020 £");
+            Map(m => m.February).Index(i++).Name("February 2020 £");
+            Map(m => m.March).Index(i++).Name("March 2020 £");
             Map(m => m.OfficialSensitive).Index(i).Name("OFFICIAL-SENSITIVE");
         }
     }
