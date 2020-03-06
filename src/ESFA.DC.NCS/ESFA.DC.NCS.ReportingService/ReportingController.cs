c823ce91eb7ca6daa4623081bf537ba9daaa7b90
Cory-Wynn <CoryWynn1993@gmail.com>
2020-03-06 10:08:08 +0000

    Dct zip name changed to just 'Reports' and .zip removed from storage to DSS

--- src/ESFA.DC.NCS/ESFA.DC.NCS.ReportingService/ReportingController.cs
+++ src/ESFA.DC.NCS/ESFA.DC.NCS.ReportingService/ReportingController.cs
@@ -42,7 +42,7 @@ namespace ESFA.DC.NCS.ReportingService
                 reportOutputFileNames.AddRange(reportsGenerated);
             }
 
-            var zipName = _filenameService.GetZipName(ncsJobContextMessage.Ukprn, ncsJobContextMessage.JobId, ncsJobContextMessage.ReportFileName);
+            var zipName = _filenameService.GetZipName(ncsJobContextMessage.Ukprn, ncsJobContextMessage.JobId, "Reports");
 
             await _zipService.CreateZipAsync(zipName, reportOutputFileNames, ncsJobContextMessage.DctContainer, cancellationToken);
 
@@ -51,7 +51,7 @@ namespace ESFA.DC.NCS.ReportingService
 
         private async Task CopyZipToDss(string dctZipName, INcsJobContextMessage ncsJobContextMessage, CancellationToken cancellationToken)
         {
-            using (var fileStream = await _dssFileService.OpenWriteStreamAsync($"{ncsJobContextMessage.ReportFileName}.zip", ncsJobContextMessage.DssContainer, cancellationToken))
+            using (var fileStream = await _dssFileService.OpenWriteStreamAsync($"{ncsJobContextMessage.ReportFileName}", ncsJobContextMessage.DssContainer, cancellationToken))
             {
                 using (var readStream = await _dctFileService.OpenReadStreamAsync(dctZipName, ncsJobContextMessage.DctContainer, cancellationToken))
                 {
